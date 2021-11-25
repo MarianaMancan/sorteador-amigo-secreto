438b309e7a1ebe14bc787cd405fb11af09cc780f
unknown <lucasfmancan@hotmail.com>
2021-11-24 21:46:58 -0300

    Ajustando sorteio

--- estudos.py
+++ estudos.py
@@ -1,11 +1,23 @@
-from random import choice
-print("Feliz Natal")
-lista = []
-while True:
-    nome = input("Digite o nome dos participantes: ")
-    lista = [nome]
-    resposta = input("Quer continuar? [S/N]").upper()
-    if resposta in "Nn":
-        break
-sorteado = (f"{choice(lista)}\n")
-print(f"O nome sorteado foi{sorteado}")
+import random 
+lista = ["TESTE1", "TESTE2", "TESTE3", "TESTE4"]
+
+# while True:
+#     nome = input("Digite um nome de participante: ")
+#     lista.append(nome)
+#     resposta = input("Quer continuar? [S/N]").upper()
+#     if resposta in "Nn":
+#         break
+
+lista.sort()
+
+conjunto = dict()
+
+while len(lista) > 0:
+    print(lista)
+    elemento = lista[0]
+ #   lista.remove(elemento)
+    par_elemento = random.choice(lista)
+    conjunto[elemento] = par_elemento
+ #   lista.remove(par_elemento)
+
+print(conjunto)
